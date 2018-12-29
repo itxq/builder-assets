1522503fa2045e2675e3b8140b81e8422734278e
IT小强xqitw.cn <360237521@qq.com>
2018-12-29 14:31:13 +0800

    优化

---
+++ assets/sortable/script.js
@@ -0,0 +1,8 @@
+/* 拖拽排序 */
+$(document).ready(function () {
+    let builderSortableItems = document.getElementsByClassName('form-json-group-core');
+    let builderSortableItemsLength = builderSortableItems.length;
+    for (let i = 0; i < builderSortableItemsLength; i++) {
+        Sortable.create(builderSortableItems[i]);
+    }
+});
