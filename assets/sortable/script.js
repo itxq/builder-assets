5ebe63d9c798955929ab0dc5ad5b4fc76e0539bb
IT小强xqitw.cn <360237521@qq.com>
2018-12-29 15:27:10 +0800

    优化

--- assets/sortable/script.js
+++ assets/sortable/script.js
@@ -3,6 +3,6 @@ $(document).ready(function () {
     let builderSortableItems = document.getElementsByClassName('form-json-group-core');
     let builderSortableItemsLength = builderSortableItems.length;
     for (let i = 0; i < builderSortableItemsLength; i++) {
-        Sortable.create(builderSortableItems[i]);
+        Sortable.create(builderSortableItems[i], {animation: 250});
     }
 });
